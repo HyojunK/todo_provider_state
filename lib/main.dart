4c2c97b1f3ecd4ea78c4fe857f9b8f7ea4e0756e
HyojunK <gywns0608@naver.com>
2023-06-29 22:38:50 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:todo_provider/pages/todos_page.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/providers/providers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        StateNotifierProvider<TodoFilter, TodoFilterState>(
+          create: (context) => TodoFilter(),
+        ),
+        StateNotifierProvider<TodoSearch, TodoSearchState>(
+          create: (context) => TodoSearch(),
+        ),
+        StateNotifierProvider<TodoList, TodoListState>(
+          create: (context) => TodoList(),
+        ),
+        StateNotifierProvider<ActiveTodoCount, ActiveTodoCountState>(
+          create: (context) => ActiveTodoCount(),
+        ),
+        StateNotifierProvider<FilteredTodos, FilteredTodosState>(
+          create: (context) => FilteredTodos(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TodosPage(),
+      ),
+    );
+  }
+}
---
+++ lib/providers/filterd_todos.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/todo_filter.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+import 'package:todo_provider/providers/todo_search.dart';
+
+class FilteredTodosState extends Equatable {
+  final List<Todo> filteredTodos;
+  FilteredTodosState({
+    required this.filteredTodos,
+  });
+
+  factory FilteredTodosState.initial() {
+    return FilteredTodosState(filteredTodos: []);
+  }
+
+  @override
+  List<Object> get props => [filteredTodos];
+
+  @override
+  bool get stringify => true;
+
+  FilteredTodosState copyWith({
+    List<Todo>? filteredTodos,
+  }) {
+    return FilteredTodosState(
+      filteredTodos: filteredTodos ?? this.filteredTodos,
+    );
+  }
+}
+
+class FilteredTodos extends StateNotifier<FilteredTodosState>
+    with LocatorMixin {
+  FilteredTodos() : super(FilteredTodosState.initial());
+
+  // FilteredTodosState get state {
+  //   List<Todo> _filteredTodos;
+
+  //   switch (todoFilter.state.filter) {
+  //     case Filter.active:
+  //       _filteredTodos =
+  //           todoList.state.todos.where((Todo todo) => !todo.completed).toList();
+  //       break;
+  //     case Filter.completed:
+  //       _filteredTodos =
+  //           todoList.state.todos.where((Todo todo) => todo.completed).toList();
+  //       break;
+  //     case Filter.all:
+  //     default:
+  //       _filteredTodos = todoList.state.todos;
+  //       break;
+  //   }
+
+  //   if (todoSearch.state.searchTerm.isNotEmpty) {
+  //     _filteredTodos = _filteredTodos
+  //         .where((Todo todo) =>
+  //             todo.desc.toLowerCase().contains(todoSearch.state.searchTerm))
+  //         .toList();
+  //   }
+
+  @override
+  void update(Locator watch) {
+    final Filter filter = watch<TodoFilterState>().filter;
+    final String searchTerm = watch<TodoSearchState>().searchTerm;
+    final List<Todo> todos = watch<TodoListState>().todos;
+
+    List<Todo> _filteredTodos;
+
+    switch (filter) {
+      case Filter.active:
+        _filteredTodos = todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        _filteredTodos = todos.where((Todo todo) => todo.completed).toList();
+        break;
+      case Filter.all:
+      default:
+        _filteredTodos = todos;
+        break;
+    }
+
+    if (searchTerm.isNotEmpty) {
+      _filteredTodos = _filteredTodos
+          .where((Todo todo) => todo.desc.toLowerCase().contains(searchTerm))
+          .toList();
+    }
+
+    state = state.copyWith(filteredTodos: _filteredTodos);
+    super.update(watch);
+  }
+}
